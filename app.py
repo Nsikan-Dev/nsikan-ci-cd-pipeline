5f1d32717b19b6754d30f22013e702f8e5020b7a
Nsikan-Dev <nsikan.udoyen@gmail.com>
2020-10-11 20:31:14 +0000

    Updated Home Page Text

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def scale(payload):
 @app.route("/")
 def home():
 
-    html = "<h3>Sklearn Prediction Home (Continuous Integration)</h3>"
+    html = "<h3>Sklearn Prediction Home (CI/CD)</h3>"
 
     return html.format(format)
 
